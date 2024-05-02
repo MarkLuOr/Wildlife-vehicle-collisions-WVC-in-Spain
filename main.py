9ce472d927a441825b995ee6d28a404e646078a7
MarkLuOr <mluengoorus@gmail.com>
2024-05-03 00:33:43 +0200

    canvis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sys
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # Es guarden les sortides en un arxiu de text
@@ -66,17 +67,99 @@ plt.tight_layout(pad=1.0)
 plt.savefig("histogrames.png") 
 plt.close() 
 
+# Gràfics de dispersió per variables numèriques
+numeric_variables = data.select_dtypes(include=['int64', 'float64']).columns
+for i, variable in enumerate(numeric_variables):
+    for j, other_variable in enumerate(numeric_variables):
+        if i != j:
+            plt.scatter(data[variable], data[other_variable])
+            plt.xlabel(variable)
+            plt.ylabel(other_variable)
+            plt.title(f"Scatter Plot: {variable} vs {other_variable}")
+            plt.tight_layout(pad=1.0) 
+            plt.savefig("gràfic_disperió_numèriques.png") 
+            plt.close()
+
+# Es seleccionen variables numèriques representatives en termes de condicions climàtiques
+selected_numeric_variables = ['tmed', 'prec', 'sol', 'altitud', 'pendiente', 'imd_total', 'maxspeed']
+
+# Es crea la matriu de dispersió 
+sns.pairplot(data[selected_numeric_variables])
+plt.savefig("scatter_matrix.png")
+plt.close()
+
+
+# Gràfics de barres apilades per les variables categòriques
+categorical_variables = data.select_dtypes(include=['object']).columns
+num_cat_vars = len(categorical_variables)
+fig, axs = plt.subplots(num_cat_vars, 1, figsize=(15, 5*num_cat_vars))
+
+for i, variable in enumerate(categorical_variables):
+    value_counts = data[variable].value_counts()
+    
+    value_counts.plot(kind='bar', stacked=True, ax=axs[i])
+    
+    # Es roten les etiquetes de x per facilitar llegibilitat
+    axs[i].set_xticklabels(axs[i].get_xticklabels(), rotation=45, ha='right')
+    
+    axs[i].set_title(f"Stacked Bar Plot for {variable}")
+    axs[i].set_xlabel(variable)
+    axs[i].set_ylabel("Count")
+
+plt.tight_layout(pad=1.0) 
+plt.savefig("barres_apilades_categòriques.png") 
+plt.close()
+
+
 
 ## Combinació de dades categòriques y numèriques:
 # El nombre total de ferits sense hospitalització per accident per dia de la setmana
 ferits_sense_hospitalitzacio_per_dia_setmana = data.groupby('nombre_dia_semana')['total_hl30df'].sum()
-print("Nombre total de morts per dia de la setmana:")
+print("Nombre total de ferits sense hospitalització per dia de la setmana:")
 print(ferits_sense_hospitalitzacio_per_dia_setmana)
 
 
+## Anàlisis de la correlació
+# Matriu de correlació entre variables numèriques
+correlation_matrix = data.corr()
+print("Matriu de correlació entre variables numèriques:")
+print(correlation_matrix)
+
+# Visualizació de la matriu de correlació amb un mapa de calor
+plt.figure(figsize=(20, 20))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title("Matriu de Correlació")
+plt.tight_layout(pad=1.0) 
+plt.savefig("Matriu de Correlació_v_num.png") 
+plt.close()
 
 
+### Anàlisis de casos en concret, per inspeccionar
 
+# Top 10 províncies amb més accidents i animal predominant
+top_10_provinciess_accidents = data['nombre_provincia'].value_counts().head(10).index.tolist()
+
+animal_predominant_per_provincia = {}
+for provincia in top_10_provinciess_accidents:
+    animal_predominant = data[data['nombre_provincia'] == provincia]['nombre_tipo_animal_1f'].mode()[0]
+    animal_predominant_per_provincia[provincia] = animal_predominant
+
+print("Top 10 províncies amb més accidents i el seu animal predominant:")
+for provincia, animal in animal_predominant_per_provincia.items():
+    print(f"Provínca: {provincia}, Animal predominant: {animal}")
+
+# Províncies de Galicia amb més accidents i animal predominant
+provincies_galicia = data[data['nombre_ccaa'] == 'Galicia']['nombre_provincia']
+top_10_provincies_galicia_accidents = provincies_galicia.value_counts().head(10).index.tolist()
+
+animal_predominant_galicia= {}
+for provincia in top_10_provincies_galicia_accidents:
+    animal_predominant = data[(data['nombre_ccaa'] == 'Galicia') & (data['nombre_provincia'] == provincia)]['nombre_tipo_animal_1f'].mode()[0]
+    animal_predominant_galicia[provincia] = animal_predominant
+
+print("\nTop 10 províncies de Galícia amb més accidents i el seu animal predominant:")
+for provincia, animal in animal_predominant_galicia.items():
+    print(f"Provincia: {provincia}, Animal predominant: {animal}")
 
 
 # Restauració del output estàndar a consola 
